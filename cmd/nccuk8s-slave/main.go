76652368e5746b5bab427070b8f6533918051886
PhilipL <honesty1997@gmail.com>
2022-05-17 16:52:59 +0800

    complete client register function

--- cmd/nccuk8s-slave/main.go
+++ cmd/nccuk8s-slave/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+var HostAddress string = "0.0.0.0"
+var HostPort string = "8888"
+
+func register() {
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", strings.Join([]string{HostAddress, HostPort}, ":"))
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		log.Fatalf("Failed to connect to %s.\n", tcpAddr)
+	}
+	defer conn.Close()
+	readbuffer := make([]byte, 16)
+	_, readerr := conn.Read(readbuffer)
+	if readerr != nil {
+		log.Fatalln("Cannot get uuid.")
+	}
+	id, err := uuid.FromBytes(readbuffer)
+	if err != nil {
+		log.Fatalln("Cannot parse uuid.")
+	}
+	log.Println(id.String())
+}
+
+// func sendHeartBeat() {
+
+// }
+
+// func executeCommand(args []string) {
+// 	command := exec.Command(args[0], args[1:]...)
+// 	for true {
+// 		if err := command.Start(); err != nil {
+// 			log.Panic(err)
+// 			break
+// 		}
+
+// 		if err := command.Wait(); err != nil {
+
+// 		}
+// 	}
+// }
 
 func main() {
-	fmt.Println("I am slave.")
+	register()
 }
